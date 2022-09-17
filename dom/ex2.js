c1a6a2cd44a5bcbd4e6a711965f84266ba5d8854
henry-hugo <henrique.hugo.9@hotmail.com>
2022-09-16 22:10:57 -0300

    selector

--- dom/ex2.js
+++ dom/ex2.js
@@ -1,3 +1,25 @@
 let btncalcular = document.getElementById('calcular');
 
-console.log(btncalcular);
+btncalcular.addEventListener('click',function(evento){
+    evento.preventDefault();
+
+    let serv = document.getElementById('servico');
+    let prazo = document.getElementById('prazo');
+    let res = document.getElementById('resultado');
+    let preco = calculapreco(serv.value, prazo.value);
+    res.innerText = `o preço e ${preco}`;
+    
+    console.log(serv.value);
+
+    console.log(evento);
+});
+
+function calculapreco(servico,prazo) {
+    let tabela =[['$$$','$$','$'],
+                ['$$$$','$$$','$$'],
+                ['--','--','$$$']];
+
+                return tabela[servico][prazo];
+}
+
+
